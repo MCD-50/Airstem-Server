ec9506df547c10f4fc22c8035a9150e6ca45683a
Manna Huizar <38449294+Jubes2011@users.noreply.github.com>
2023-10-26 01:17:20 -0500

    Update match.js

--- src/engine/match.js
+++ src/engine/match.js
@@ -6,16 +6,18 @@ import { match as mp3pm_match } from '../providers/match/mp3pm';
 import { match as pleer_match } from '../providers/match/pleer';
 import { match as soundcloud_match } from '../providers/match/soundcloud';
 import { match as youtube_match, match_fast } from '../providers/match/youtube';
+import { match as spotify_match, match_fast } from'../providers/match/spotify';
 
 
 export const match = (opts, callback) => {
 	parallel({
 		//mimp3_parallel: x => mimp3_match(opts, x),
 		//mp3cold_parallel: y => mp3cold_match(opts, y),
-		mp3pm_parallel: z => mp3pm_match(opts, z),
+		//mp3pm_parallel: z => mp3pm_match(opts, z),
 		//pleer_parallel: x1 => pleer_match(opts, x1),
-		youtube_parallel: y1 => youtube_match(opts, y1),
-		soundcloud_parallel: z1 => soundcloud_match(opts, z1),
+		//youtube_parallel: y1 => youtube_match(opts, y1),
+		//soundcloud_parallel: z1 => soundcloud_match(opts, z1),
+		//spotify_parallel: x2 => spotify_match(opts, x2),
 	}, (err, res) => {
 		let messages = [];
 		if (!err && res) {
